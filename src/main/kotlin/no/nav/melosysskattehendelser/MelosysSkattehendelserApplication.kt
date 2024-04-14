838c8dfe72ea035bcd9e0a3cea690aede347bd4e
Eirik Bergande <eirik.bergande@nav.no>
2024-04-14 14:07:14 +0200

    Opprettet skall

---
+++ src/main/kotlin/no/nav/melosysskattehendelser/MelosysSkattehendelserApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.melosysskattehendelser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MelosysSkattehendelserApplication
+
+fun main(args: Array<String>) {
+    runApplication<MelosysSkattehendelserApplication>(*args)
+}
