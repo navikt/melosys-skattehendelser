e8c0eba7e5d04b2817a3188a82bcc059200d0fae
Eirik Bergande <eirik.bergande@nav.no>
2024-05-07 23:17:27 +0200

    legger til actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
