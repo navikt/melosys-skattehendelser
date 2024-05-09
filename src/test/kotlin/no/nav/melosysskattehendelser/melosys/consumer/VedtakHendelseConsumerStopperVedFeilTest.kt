2e51e1b3e75ba46940931f9fc69212074a0b177e
RuneLind <rulind@gmail.com>
2024-05-09 12:13:44 +0200

    Sjekk at vi stopper kafka Consumer ved feil

--- src/test/kotlin/no/nav/melosysskattehendelser/melosys/consumer/VedtakHendelseConsumerStopperVedFeilTest.kt
+++ src/test/kotlin/no/nav/melosysskattehendelser/melosys/consumer/VedtakHendelseConsumerStopperVedFeilTest.kt
@@ -1,42 +1,32 @@
 package no.nav.melosysskattehendelser.melosys.consumer
 
+import ch.qos.logback.classic.Level
+import io.kotest.matchers.shouldBe
+import no.nav.melosysskattehendelser.LoggingTestUtils
+import no.nav.melosysskattehendelser.melosys.KafkaTestProducer
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.untilNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.context.annotation.Import
+import org.springframework.kafka.KafkaException
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.TimeUnit
 
 @ActiveProfiles("test")
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(count = 1, controlledShutdown = true, partitions = 1)
+@Import(KafkaTestProducer::class)
 class VedtakHendelseConsumerStopperVedFeilTest(
     @Autowired private val kafkaTemplate: KafkaTemplate<String, String>,
     @Value("\${melosys.kafka.consumer.topic}") private val topic: String
 ) {
-    @TestConfiguration
-    class Config {
-        @Bean
-        fun testKafkaTemplate(
-            kafkaProperties: KafkaProperties,
-        ): KafkaTemplate<String, String> {
-            val props =
-                kafkaProperties.buildProducerProperties(null) +
-                        mapOf(
-                            "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
-                            "value.serializer" to "org.apache.kafka.common.serialization.StringSerializer"
-                        )
-            return KafkaTemplate(DefaultKafkaProducerFactory(props))
-        }
-    }
-
     @Test
     fun `skal feile ved ugyldlig json`() {
         val meldingMedFeil = """
@@ -47,10 +37,19 @@ class VedtakHendelseConsumerStopperVedFeilTest(
                 }
             }"""
 
-        kafkaTemplate.send(topic, meldingMedFeil)
 
+        LoggingTestUtils.withLogCapture { logItems ->
+            kafkaTemplate.send(topic, meldingMedFeil)
 
-        Thread.sleep(2000)
-
+            val errorLogItem = await
+                .timeout(5, TimeUnit.SECONDS)
+                .untilNotNull {
+                    logItems.firstOrNull() { it.level == Level.ERROR }
+                }
+            errorLogItem.throwableProxy.apply {
+                className shouldBe KafkaException::class.qualifiedName
+                message.shouldBe("Stopped container")
+            }
+        }
     }
 }
