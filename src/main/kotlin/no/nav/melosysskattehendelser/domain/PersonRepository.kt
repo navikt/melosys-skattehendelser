061ad271bf35280ab9ef9bfe0614a68c36dcbcc2
RuneLind <rulind@gmail.com>
2024-05-15 16:21:01 +0200

    Legg findPersonByIdent til PersonRepository

--- src/main/kotlin/no/nav/melosysskattehendelser/domain/PersonRepository.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/domain/PersonRepository.kt
@@ -3,5 +3,5 @@ package no.nav.melosysskattehendelser.domain
 import org.springframework.data.repository.CrudRepository
 
 interface PersonRepository : CrudRepository<Person, Long> {
-
+    fun findPersonByIdent(ident: String): Person?
 }
