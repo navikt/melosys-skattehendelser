630ab7cf244c13665d6265bfcf808a896b750eda
RuneLind <rulind@gmail.com>
2024-05-15 09:07:03 +0200

    Legg på StartupRunner kun for raskere testing av sigrun api

---
+++ src/main/kotlin/no/nav/melosysskattehendelser/melosys/StartupRunner.kt
@@ -0,0 +1,17 @@
+package no.nav.melosysskattehendelser.melosys
+
+import no.nav.melosysskattehendelser.skatt.SkattHendelserFetcher
+import org.springframework.stereotype.Component
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+
+/***
+ * Brukt for raskere testing av sigrun q2/ske, skal fjernes
+ */
+@Component
+class StartupRunner(private val skattHendelserFetcher: SkattHendelserFetcher) : ApplicationListener<ApplicationReadyEvent> {
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        val count = skattHendelserFetcher.hentHendelser(2000).count()
+        println("Hentet $count hendelser fra startsekvensnummer 2000")
+    }
+}
