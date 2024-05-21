30f41dbaf7c66918dbe9575ebe347ee106fdd9fe
RuneLind <rulind@gmail.com>
2024-05-21 16:06:21 +0200

    Legg batch-size inn i application.yaml

--- src/main/kotlin/no/nav/melosysskattehendelser/skatt/SkatteHendelserFetcher.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/skatt/SkatteHendelserFetcher.kt
@@ -1,12 +1,20 @@
 package no.nav.melosysskattehendelser.skatt
 
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 
+private val log = KotlinLogging.logger { }
+
 @Component
-class SkatteHendelserFetcher(private val skatteHendelseConsumer: SkatteHendelseConsumer) {
-    private val log = KotlinLogging.logger { }
+class SkatteHendelserFetcher(
+    private val skatteHendelseConsumer: SkatteHendelseConsumer,
+    @Value("\$skatt.fetcher.batch-size") private val batchSize: Int
+) {
+    init {
+        log.info("batchSize er satt til $batchSize")
+    }
 
     fun hentHendelser(startSeksvensnummer: Long, batchDone: (seksvensnummer: Long) -> Unit) = sequence<Hendelse> {
         var seksvensnummerFra = startSeksvensnummer
@@ -14,7 +22,7 @@ class SkatteHendelserFetcher(private val skatteHendelseConsumer: SkatteHendelseC
         var totaltAntallHendelser = 0
         do {
             hendelseListe = hentSkatteHendelseser(seksvensnummerFra)
-            if (hendelseListe.size > BATCH_SIZE) error("hendelseListe.size${hendelseListe.size} > batchSize$BATCH_SIZE")
+            if (hendelseListe.size > batchSize) error("hendelseListe.size ${hendelseListe.size} > batchSize $batchSize")
             val last = hendelseListe.lastOrNull() ?: break
             log.info(
                 "Hentet ${hendelseListe.size} hendelser fra sekvensnummer $seksvensnummerFra til ${last.sekvensnummer} " +
@@ -24,17 +32,17 @@ class SkatteHendelserFetcher(private val skatteHendelseConsumer: SkatteHendelseC
             yieldAll(hendelseListe)
             batchDone(seksvensnummerFra)
             totaltAntallHendelser += hendelseListe.size
-            if (hendelseListe.size < BATCH_SIZE) {
+            if (hendelseListe.size < batchSize) {
                 break
             }
-        } while (hendelseListe.size == BATCH_SIZE)
+        } while (hendelseListe.size == batchSize)
         log.info("totalt antall hendelser prossessert: $totaltAntallHendelser seksvensnummerFra er nå: $seksvensnummerFra")
     }
 
     private fun hentSkatteHendelseser(seksvensnummerFra: Long) = skatteHendelseConsumer.hentHendelseListe(
         HendelseRequest(
             seksvensnummerFra = seksvensnummerFra,
-            antall = BATCH_SIZE,
+            antall = batchSize,
             brukAktoerId = false
         )
     )
@@ -49,8 +57,4 @@ class SkatteHendelserFetcher(private val skatteHendelseConsumer: SkatteHendelseC
                     log.info("start dato for startSekvensnummer er nå hardkodet til $this")
                 }
         )
-
-    companion object {
-        const val BATCH_SIZE = 500
-    }
 }
