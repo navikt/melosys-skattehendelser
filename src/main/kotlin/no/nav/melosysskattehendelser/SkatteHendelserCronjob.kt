0a3cb925a53e1c5e65f29b2e0ea19f2bce942622
RuneLind <rulind@gmail.com>
2024-05-23 15:30:55 +0200

    Fjern mulighet for å sette Cron expression dynamisk
    Den settes kun etter at forrige er ferdig så ikke så mye nytte

--- src/main/kotlin/no/nav/melosysskattehendelser/SkatteHendelserCronjob.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/SkatteHendelserCronjob.kt
@@ -1,43 +1,18 @@
 package no.nav.melosysskattehendelser
 
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.scheduling.Trigger
-import org.springframework.scheduling.annotation.SchedulingConfigurer
-import org.springframework.scheduling.config.ScheduledTaskRegistrar
-import org.springframework.scheduling.support.CronTrigger
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.Executors
 
 private val log = KotlinLogging.logger { }
 
 @Component
 class SkatteHendelserCronjob(
-    private val skatteHendelsePublisering: SkatteHendelsePublisering,
-    @Value("\${cron.job.prosesser-skatt-hendelser}") var cronExpression: String
-) : SchedulingConfigurer {
-
-    init {
-        log.info("Cronjob for å hente og publisere skattehendelser er satt til $cronExpression")
-    }
-
-    override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
-        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(1))
-        taskRegistrar.addTriggerTask(
-            { startHendelseProsessering() }, customTrigger()
-        )
-    }
-
-    private fun customTrigger(): Trigger {
-        return Trigger { triggerContext ->
-            CronTrigger(cronExpression).nextExecution(triggerContext).apply {
-                log.info("Next execution time of the cron job is set to $this")
-            }
-        }
-    }
-
+    private val skatteHendelsePublisering: SkatteHendelsePublisering
+) {
+    @Scheduled(cron = "\${cron.job.prosesser-skatt-hendelser}")
     fun startHendelseProsessering() {
-        log.info("Kjører cronjob for å hente og publisere skattehendelser") // (  )
+        log.info("Kjører cronjob for å hente og publisere skattehendelser")
         skatteHendelsePublisering.prosesserSkattHendelser()
     }
 }
