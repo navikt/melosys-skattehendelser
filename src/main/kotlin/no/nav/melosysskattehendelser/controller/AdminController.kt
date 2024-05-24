c02b9e348a4085adff6d451c53718c63d228300b
RuneLind <rulind@gmail.com>
2024-05-24 15:58:37 +0200

    Gjør AdminController @Unprotected
    Skal være protected, men åpner for testing på q2 før vi har lagt på melosys innlogging eller noe

--- src/main/kotlin/no/nav/melosysskattehendelser/controller/AdminController.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/controller/AdminController.kt
@@ -2,12 +2,13 @@ package no.nav.melosysskattehendelser.controller
 
 import no.nav.melosysskattehendelser.SkatteHendelsePublisering
 import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@Protected
+@Unprotected // TODO: Skal være protected, men åpner for testing på q2 før vi har lagt på melosys innlogging eller noe
 @RequestMapping("/admin")
 class AdminController(
     private val skatteHendelsePublisering: SkatteHendelsePublisering
