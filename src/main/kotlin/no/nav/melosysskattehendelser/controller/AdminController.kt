a6786fbb0d67209023ca02e58f77bcf2686a8107
RuneLind <rulind@gmail.com>
2024-05-29 08:45:38 +0200

    Legg til api-key

--- src/main/kotlin/no/nav/melosysskattehendelser/controller/AdminController.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/controller/AdminController.kt
@@ -1,31 +1,43 @@
 package no.nav.melosysskattehendelser.controller
 
 import no.nav.melosysskattehendelser.SkatteHendelsePublisering
-import no.nav.security.token.support.core.api.Protected
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@Unprotected // TODO: Skal være protected, men åpner for testing på q2 før vi har lagt på melosys innlogging eller noe
+@Unprotected
 @RequestMapping("/admin")
 class AdminController(
-    private val skatteHendelsePublisering: SkatteHendelsePublisering
+    private val skatteHendelsePublisering: SkatteHendelsePublisering,
+    @Value("\${admin.api-key}") private val apiKey: String
 ) {
     @PostMapping("/hendelseprosessering/start")
-    fun startHendelseProsessering(): ResponseEntity<String> {
+    fun startHendelseProsessering(@RequestHeader(API_KEY_HEADER) apiKey: String): ResponseEntity<String> {
+        validerApiKey(apiKey)
         skatteHendelsePublisering.asynkronProsesseringAvSkattHendelser()
         return ResponseEntity.ok("Hendelseprosessering startet")
     }
 
     @PostMapping("/hendelseprosessering/stop")
-    fun stopHendelseProsessering(): ResponseEntity<String> {
+    fun stopHendelseProsessering(@RequestHeader(API_KEY_HEADER) apiKey: String): ResponseEntity<String> {
         skatteHendelsePublisering.stopProsesseringAvSkattHendelser()
         return ResponseEntity.ok("Hendelseprosessering stoppet")
     }
 
     @GetMapping("/hendelseprosessering/status")
-    fun status(): ResponseEntity<Map<String, Any>> =
-        ResponseEntity<Map<String, Any>>(skatteHendelsePublisering.status(), HttpStatus.OK)
+    fun status(@RequestHeader(API_KEY_HEADER) apiKey: String): ResponseEntity<Map<String, Any>> {
+        validerApiKey(apiKey)
+        return ResponseEntity<Map<String, Any>>(skatteHendelsePublisering.status(), HttpStatus.OK)
+    }
+
+    private fun validerApiKey(apiKey: String) {
+        require(apiKey == this.apiKey) { "Ugyldig apikey" }
+    }
+
+    companion object {
+        const val API_KEY_HEADER = "X-SKATTEHENDELSER-ADMIN-APIKEY"
+    }
 }
