8eb113042dd83cbd532696124ddc1fc59dd484ee
RuneLind <rulind@gmail.com>
2024-05-30 11:51:51 +0200

    TestAuthController skal ikke gjøre noe annet en å gi response ok

--- src/main/kotlin/no/nav/melosysskattehendelser/controller/TestAuthController.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/controller/TestAuthController.kt
@@ -1,6 +1,5 @@
 package no.nav.melosysskattehendelser.controller
 
-import no.nav.melosysskattehendelser.SkatteHendelsePublisering
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -8,12 +7,9 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @Protected
 @RequestMapping("/auth")
-class TestAuthController(
-    private val skatteHendelsePublisering: SkatteHendelsePublisering
-) {
+class TestAuthController {
     @GetMapping("/test")
     fun testAuth(): ResponseEntity<String> {
-        skatteHendelsePublisering.asynkronProsesseringAvSkattHendelser()
         return ResponseEntity.ok("ok")
     }
 }
