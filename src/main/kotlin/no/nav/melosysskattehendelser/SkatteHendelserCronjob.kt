3883abc2b05cad094fa8976f3cef59ac78473694
Eirik Bergande <eirik.bergande@nav.no>
2024-06-21 09:28:24 +0200

    6677 Legger på shedlock for hent skattehendelser cron job (#12)
    * Legger på shedlock for hent skattehendelser cron job
    
    * scheduler lockForAtMost 12h

--- src/main/kotlin/no/nav/melosysskattehendelser/SkatteHendelserCronjob.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/SkatteHendelserCronjob.kt
@@ -1,6 +1,7 @@
 package no.nav.melosysskattehendelser
 
 import mu.KotlinLogging
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -11,6 +12,7 @@ class SkatteHendelserCronjob(
     private val skatteHendelsePublisering: SkatteHendelsePublisering
 ) {
     @Scheduled(cron = "\${cron.job.prosesser-skatt-hendelser}")
+    @SchedulerLock(name = "hent skattehendelser", lockAtMostFor = "PT12H")
     fun startHendelseProsessering() {
         log.info("Kjører cronjob for å hente og publisere skattehendelser")
         skatteHendelsePublisering.prosesserSkattHendelser()
