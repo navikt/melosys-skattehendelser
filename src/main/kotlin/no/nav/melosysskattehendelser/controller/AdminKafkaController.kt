c396d3b761b3335fa602dfbdb23fec3dc477f9f6
Rune Lind <rune.lind@nav.no>
2025-03-25 21:59:30 +0100

    7199 mulighet for manuelt sende melosys melding (#45)
    Legg til AdminKafkaController

---
+++ src/main/kotlin/no/nav/melosysskattehendelser/controller/AdminKafkaController.kt
@@ -0,0 +1,28 @@
+package no.nav.melosysskattehendelser.controller
+
+import mu.KotlinLogging
+import no.nav.melosysskattehendelser.melosys.MelosysSkatteHendelse
+import no.nav.melosysskattehendelser.melosys.producer.SkattehendelserProducer
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger { }
+
+@RestController
+@Unprotected
+@RequestMapping("/admin/kafka")
+class AdminKafkaController(
+    private val skattehendelserProducer: SkattehendelserProducer
+) {
+    @PostMapping("/sendMelosysHendelse")
+    fun sendMessage(@RequestBody hendelse: MelosysSkatteHendelse): ResponseEntity<String> {
+        log.info("send melding: $hendelse")
+        skattehendelserProducer.publiserMelding(hendelse)
+
+        return ResponseEntity.ok("melding sent")
+    }
+}
