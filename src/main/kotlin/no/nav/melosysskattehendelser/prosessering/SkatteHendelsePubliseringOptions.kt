30f5c32b2b1ce4fc2f367c969f1011d24144a688
RuneLind <rulind@gmail.com>
2025-04-07 21:13:13 +0200

    Legg til JobConfirmationService
    Som stopper at vi kjører job i prod med en feil

--- src/main/kotlin/no/nav/melosysskattehendelser/prosessering/SkatteHendelsePubliseringOptions.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/prosessering/SkatteHendelsePubliseringOptions.kt
@@ -1,22 +1,6 @@
 package no.nav.melosysskattehendelser.prosessering
 
-import mu.KotlinLogging
-import org.springframework.core.env.Environment
-
-private val log = KotlinLogging.logger { }
-
 data class SkatteHendelsePubliseringOptions(
-    val useCache: Boolean = false
-) {
-    companion object {
-        fun av(environment: Environment): SkatteHendelsePubliseringOptions {
-            val naisClusterName = environment.getProperty(NAIS_CLUSTER_NAME)
-            val isProd: Boolean = naisClusterName?.contains("prod") == true
-            log.info { "NAIS_CLUSTER_NAME=$naisClusterName, isProd=$isProd" }
-
-            return SkatteHendelsePubliseringOptions()
-        }
-
-        const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
-    }
-}
+    val useCache: Boolean = false,
+    val confirmationCode: String? = null
+)
