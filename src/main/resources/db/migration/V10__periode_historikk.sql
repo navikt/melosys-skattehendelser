06195a671e55b3df99407dda7c750c5b0da8b3b6
RuneLind <rulind@gmail.com>
2025-06-26 16:22:45 +0200

    Legg til duplikat sjekk i db for publiserings_historikk

--- src/main/resources/db/migration/V10__periode_historikk.sql
+++ src/main/resources/db/migration/V10__periode_historikk.sql
@@ -3,6 +3,7 @@ CREATE TABLE publiserings_historikk
     id                 BIGSERIAL PRIMARY KEY,
     periode_id         BIGINT    NOT NULL,
     sekvensnummer      BIGINT    NOT NULL,
-    siste_Hendelse_tid TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (periode_id) REFERENCES periode (id)
+    siste_hendelse_tid TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (periode_id) REFERENCES periode (id),
+    CONSTRAINT unique_periode_sekvensnummer UNIQUE (periode_id, sekvensnummer)
 );
