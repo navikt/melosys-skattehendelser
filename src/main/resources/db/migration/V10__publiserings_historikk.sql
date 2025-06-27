a8823e4730293ffc110ee224695ce020121e7e60
RuneLind <rulind@gmail.com>
2025-06-27 15:24:24 +0200

    publiserings_historikk ikke kunne ha duplikat sekvensnummer

--- src/main/resources/db/migration/V10__publiserings_historikk.sql
+++ src/main/resources/db/migration/V10__publiserings_historikk.sql
@@ -6,5 +6,5 @@ CREATE TABLE publiserings_historikk
     sekvensnummer      BIGINT     NOT NULL,
     siste_hendelse_tid TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (periode_id) REFERENCES periode (id),
-    CONSTRAINT unique_periode_sekvensnummer UNIQUE (periode_id, sekvensnummer)
+    CONSTRAINT unique_periode_sekvensnummer UNIQUE (sekvensnummer)
 );
