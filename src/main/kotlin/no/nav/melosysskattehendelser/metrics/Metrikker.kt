9458930a2783fc8f9150ff868b008286d2dff483
RuneLind <rulind@gmail.com>
2025-06-30 16:13:39 +0200

    Init og endre navn fra periode til inntektsår for metrikk

--- src/main/kotlin/no/nav/melosysskattehendelser/metrics/Metrikker.kt
+++ src/main/kotlin/no/nav/melosysskattehendelser/metrics/Metrikker.kt
@@ -16,6 +16,7 @@ class Metrikker {
         Metrics.counter(PUBLISERING_FEILET)
         Metrics.counter(VEDTAK_MOTTATT_OG_PERSON_LAGT_TIL)
         Metrics.counter(VEDTAK_MOTTATT_OG_PERIODE_LAGT_TIL)
+        Metrics.counter(HENDELSE_FLERE_PUBLISERINGER_PR_INNTEKTSÅR)
     }
 
     fun hendelseHentet() = Metrics.counter(HENDELSE_HENTET).increment()
@@ -24,7 +25,7 @@ class Metrikker {
 
     fun hendelsePublisert() = Metrics.counter(HENDELSE_PUBLISERT).increment()
 
-    fun hendelseFlerePublisertPrPeriode() = Metrics.counter(HENDELSE_FLERE_PUBLISERINGER_PR_PERIODE).increment()
+    fun hendelseFlerePublisertPrInntektsÅr() = Metrics.counter(HENDELSE_FLERE_PUBLISERINGER_PR_INNTEKTSÅR).increment()
 
     fun duplikatHendelse() = Metrics.counter(DUPLIKAT_HENDELSE).increment()
 
@@ -40,7 +41,7 @@ class Metrikker {
         private const val HENDELSE_HENTET = "$METRIKKER_NAMESPACE.hendelse.hentet"
         private const val PERSON_FUNNET = "$METRIKKER_NAMESPACE.person.funnet"
         private const val HENDELSE_PUBLISERT = "$METRIKKER_NAMESPACE.hendelse.publisert"
-        private const val HENDELSE_FLERE_PUBLISERINGER_PR_PERIODE = "$METRIKKER_NAMESPACE.hendelser.flere.publisert.pr.periode"
+        private const val HENDELSE_FLERE_PUBLISERINGER_PR_INNTEKTSÅR = "$METRIKKER_NAMESPACE.hendelser.flere.publisert.pr.inntektsaar"
         private const val DUPLIKAT_HENDELSE = "$METRIKKER_NAMESPACE.duplikat.hendelse"
         private const val PUBLISERING_FEILET = "$METRIKKER_NAMESPACE.publisering.feilet"
         private const val VEDTAK_MOTTATT_OG_PERSON_LAGT_TIL = "$METRIKKER_NAMESPACE.vedtak.mottatt.og.person.lagt.til"
