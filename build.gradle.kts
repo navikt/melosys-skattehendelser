7291711b38d8a6e78e74b91c88a03ebf65c2d21d
RuneLind <rulind@gmail.com>
2025-11-17 21:00:08 +0100

    Nedgrader til 2.2.0 for at ting skal virke i intellj

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.2.20"
-    kotlin("plugin.spring") version "2.2.20"
-    kotlin("plugin.jpa") version "2.2.20"
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
+    kotlin("plugin.jpa") version "2.2.0"
 }
 
 group = "no.nav.melosys"
